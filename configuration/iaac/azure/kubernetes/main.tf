f419b5e9a0f53636f277b5dfcbf0698fb4f2a0b6
“emilesherrott” <emilesherrott@gmail.com>
2024-02-29 21:51:43 +0000

    Test apply

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "standard_dc4ads_v5"
+    vm_size         = "standard_DC2s_v2"
     # vm_size = "standard_dc2ads_v5" 6
     # Choose VM: https://azure.microsoft.com/en-gb/pricing/vm-selector/
   }
