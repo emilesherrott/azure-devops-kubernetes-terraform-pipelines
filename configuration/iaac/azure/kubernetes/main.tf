9ebc34a3c1b366d61d9528b058673981c0ffd6ef
“emilesherrott” <emilesherrott@gmail.com>
2025-05-01 10:18:38 +0100

    Update image value

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -39,6 +39,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   }
 }
 
+
 terraform {
   backend "azurerm" {
     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
